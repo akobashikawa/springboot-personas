363808982e139842ac330800fd9d4cc28012d78c
Antonio Kobashikawa <akobashikawa@gmail.com>
2023-02-22 20:07:11 -0500

    endpoint hola implementado

--- src/main/java/com/example/personas/PersonaController.java
+++ src/main/java/com/example/personas/PersonaController.java
@@ -4,6 +4,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,6 +16,14 @@ public class PersonaController {
     public Map<String, String> holamundo() {
         HashMap<String, String> map = new HashMap<>();
         map.put("saludo", "Hola Mundo!");
+        return map;
+    }
+
+    @PostMapping("/hola")
+    public Map<String, String> hola(@RequestBody Persona persona) {
+        HashMap<String, String> map = new HashMap<>();
+        String nombre = persona.getNombre();
+        map.put("saludo", "Hola " + nombre + "!");
         return map;
     }
 }
