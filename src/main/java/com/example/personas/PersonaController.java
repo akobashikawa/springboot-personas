f50e9ca45a38c4cab61c7b9e71804bf65f2b39f6
Antonio Kobashikawa <akobashikawa@gmail.com>
2023-02-22 21:05:03 -0500

    hola actualizado

--- src/main/java/com/example/personas/PersonaController.java
+++ src/main/java/com/example/personas/PersonaController.java
@@ -3,27 +3,31 @@ package com.example.personas;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import antlr.collections.List;
+
 @RestController
 public class PersonaController {
-    
+
     @GetMapping("/holamundo")
-    public Map<String, String> holamundo() {
+    public HashMap<String, String> holamundo() {
         HashMap<String, String> map = new HashMap<>();
         map.put("saludo", "Hola Mundo!");
         return map;
     }
 
     @PostMapping("/hola")
-    public Map<String, String> hola(@RequestBody Persona persona) {
+    public HashMap<String, String> hola(@RequestBody Persona persona) {
         HashMap<String, String> map = new HashMap<>();
-        String nombre = persona.getNombre();
+        String nombre = persona.getNombre() == "" ? "Mundo" : persona.getNombre();
         map.put("saludo", "Hola " + nombre + "!");
         return map;
     }
+
 }
