ccbfe6ad2cac0a8302c892a3577d3a03351990fb
Antonio Kobashikawa <akobashikawa@gmail.com>
2023-02-26 21:39:35 -0500

    Cambia ruta, implementa cors

--- src/main/java/com/example/personas/PersonaController.java
+++ src/main/java/com/example/personas/PersonaController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class PersonaController {
 
@@ -23,7 +25,7 @@ public class PersonaController {
     @Autowired
     private PersonaService personaService;
 
-    @GetMapping("/holamundo")
+    @GetMapping("/hola")
     public HashMap<String, String> holamundo() {
         HashMap<String, String> map = new HashMap<>();
         String saludo = holaService.getSaludo("");
