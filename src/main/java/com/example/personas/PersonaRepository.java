ef6dfdb52f8412b71c57393c3ac9c0b2c35fc180
Antonio Kobashikawa <akobashikawa@gmail.com>
2023-03-09 20:21:25 -0500

    Usando interface e implementacion

--- src/main/java/com/example/personas/PersonaRepository.java
+++ src/main/java/com/example/personas/PersonaRepository.java
@@ -1,43 +1,12 @@
 package com.example.personas;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class PersonaRepository {
+public interface PersonaRepository {
     
-    private List<Persona> personas = new ArrayList<>();
-    private Long idActual = 0L;
-
-    public List<Persona> findAll() {
-        return personas;
-    }
-
-    public Persona save(Persona persona) {
-        if (persona.getId() == null) {
-            idActual++;
-            persona.setId(idActual);
-            personas.add(persona);
-        } else {
-            int index = personas.indexOf(persona);
-            if (index >= 0) {
-                personas.set(index, persona);
-            } else {
-                personas.add(persona);
-            }
-        }
-        return persona;
-    }
-
-    public Optional<Persona> findById(Long id) {
-        return personas.stream().filter(p -> p.getId().equals(id)).findFirst();
-    }
-
-    public void deleteById(Long id) {
-        personas.removeIf(p -> p.getId().equals(id));
-    }
-
+    List<Persona> findAll();
+    Persona save(Persona persona);
+    Optional<Persona> findById(Long id);
+    void deleteById(Long id);
 }
