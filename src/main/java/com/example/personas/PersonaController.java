585db3dc0e05effca9a64cf9bbf133694ce783d3
Antonio Kobashikawa <akobashikawa@gmail.com>
2023-04-05 17:05:23 -0500

    Agregado cors

--- src/main/java/com/example/personas/PersonaController.java
+++ src/main/java/com/example/personas/PersonaController.java
@@ -3,11 +3,13 @@ package com.example.personas;
 import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class PersonaController {
 
