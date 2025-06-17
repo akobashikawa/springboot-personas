4fb115ebbaaae5dc48c0e5fc5dcce2d245cebe17
Antonio Kobashikawa <akobashikawa@gmail.com>
2025-06-17 16:22:21 -0500

    Usa ResponseEntity

--- src/main/java/com/example/personas/PersonaController.java
+++ src/main/java/com/example/personas/PersonaController.java
@@ -1,8 +1,10 @@
 package com.example.personas;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,20 +19,20 @@ public class PersonaController {
     private HolaService holaService;
 
     @GetMapping("/holamundo")
-    public HashMap<String, String> holamundo() {
-        HashMap<String, String> map = new HashMap<>();
+    public ResponseEntity<Map<String, String>> holamundo() {
+        Map<String, String> response = new HashMap<>();
         String saludo = holaService.getSaludo("");
-        map.put("saludo", saludo);
-        return map;
+        response.put("saludo", saludo);
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/hola")
-    public HashMap<String, String> hola(@RequestBody Persona persona) {
-        HashMap<String, String> map = new HashMap<>();
+    public ResponseEntity<HashMap<String, String>> hola(@RequestBody Persona persona) {
+        HashMap<String, String> response = new HashMap<>();
         String nombre = persona.getNombre();
         String saludo = holaService.getSaludo(nombre);
-        map.put("saludo", saludo);
-        return map;
+        response.put("saludo", saludo);
+        return ResponseEntity.ok(response);
     }
 
 }
